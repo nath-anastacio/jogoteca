e571acd5bcbe733bca64b1216173c523a5eef724
Nathalia <nathanastacio@gmail.com>
2025-02-26 14:57:00 -0300

    Corrigindo erros

--- helpers.py
+++ helpers.py
@@ -5,8 +5,8 @@ from wtforms import StringField, validators, SubmitField, PasswordField
 
 class FormularioJogo(FlaskForm):
     nome = StringField('Nome do Jogo', [validators.DataRequired(), validators.Length(min=1, max=50)])
-    categoria = StringField('Nome do Jogo', [validators.DataRequired(), validators.Length(min=1, max=400)])
-    console = StringField('Nome do Jogo', [validators.DataRequired(), validators.Length(min=1, max=20)])
+    categoria = StringField('Categoria', [validators.DataRequired(), validators.Length(min=1, max=400)])
+    console = StringField('Console', [validators.DataRequired(), validators.Length(min=1, max=20)])
     salvar = SubmitField('Salvar')
 
 class FormularioUsuario(FlaskForm):
